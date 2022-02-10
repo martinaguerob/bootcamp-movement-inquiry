fe78c155b53ce1c92bf1301290534aff860f87eb
Martin Aguero <maguerob@nttdata.com>
2022-02-10 01:15:27 -0500

    first commit

---
+++ src/main/java/com/nttdata/movementinquiry/MovementInquiryApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.movementinquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementInquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementInquiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/movementinquiry/MovementInquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.movementinquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementInquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
